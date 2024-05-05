9fdaf6f5cc8268ea9936294cc14f46d21047806c
Elian Aguilera <140450840+ElianAguilera@users.noreply.github.com>
2024-05-05 01:27:09 -0500

    Create globalVars.h

---
+++ week 13 Coding Class/globalVars.h
@@ -0,0 +1,8 @@
+#include "includes.h"
+
+// using a collection initializer to add the values.
+const string weaponNames[] = {"Sting", "Excalibur"};
+
+enum difficulty { EASY = 1, MEDIUM, HARD };
+
+const float earthGravity = -9.81f;
